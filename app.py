3f3631be24231501f43a2e45fa97a2fd79622766
cruelkratos <mailgarvsethi@gmail.com>
2025-09-23 18:08:11 +0530

    check user exists

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask , jsonify , json , request
 from flask_cors import CORS
-from helper.firebase import getUsers , _getMetaData , _getQuestions
+from helper.firebase import getUsers , _getMetaData , _getQuestions , checkUserExists
 from helper.middleware import authenticate_request
 from routes.user_routing import user_bp
 app = Flask(__name__)
@@ -25,14 +25,14 @@ def health_check():
 @app.route('/get-questions' , methods = ['GET'])
 @authenticate_request
 def getQuestions():
-    attributes = request.args 
+	attributes = request.args 
 
-    required_params = ['subject', 'subcategory', 'standard', 'difficulty']
-    if not all(param in attributes for param in required_params):
-        return {'_error' : 'Missing one or more required query parameters' }, 422
-    
-    questions_list = _getQuestions(attributes)
-    return jsonify(questions_list)
+	required_params = ['subject', 'subcategory', 'standard', 'difficulty']
+	if not all(param in attributes for param in required_params):
+		return {'_error' : 'Missing one or more required query parameters' }, 422
+	
+	questions_list = _getQuestions(attributes)
+	return jsonify(questions_list)
 
 @app.route('/get-metadata' , methods = ['GET'])
 @authenticate_request
@@ -40,6 +40,14 @@ def getMetaData():
 	if _getMetaData() is not None:
 		return jsonify(_getMetaData())
 	return {'_error:' "No MetaData was found"} , 404
+
+@app.route('/check-user-exists' , methods = ['GET'])
+@authenticate_request
+def check_user_exists():
+	arguments = request.args
+	_uid = arguments.get('uid')
+	exists = checkUserExists(user_id=_uid)
+	return jsonify({'exists': exists}), 200
 
 
 @app.route('/users' , methods = ['GET'])
